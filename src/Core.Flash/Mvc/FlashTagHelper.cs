6c01e95a93d42f87f447e18986e1e39db57d2f4c
Serhii Prostakov <serhii.prostakov@gmail.com>
2018-07-28 21:07:30 +0300

    Fixed dismissable flash messages functionality.

--- src/Core.Flash/Mvc/FlashTagHelper.cs
+++ src/Core.Flash/Mvc/FlashTagHelper.cs
@@ -12,6 +12,15 @@ namespace Core.Flash.Mvc
     {
         private ITempDataDictionary tempData;
 
+        private const string HtmlStandardTemplate = "<div class=\"alert alert-{0}\">{1}</div>";
+
+        private const string HtmlDismissableTemplate = 
+            "<div class=\"alert alert-{0} alert-dismissible\" role=\"alert\">" +
+                "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">" +
+                    "<span aria-hidden=\"true\">&times;</span>" +
+                "</button>{1}" +
+            "</div>";
+
         public FlashesTagHelper(ITempDataDictionaryFactory factory, IHttpContextAccessor contextAccessor)
         {
             tempData = factory.GetTempData(contextAccessor.HttpContext);
@@ -24,7 +33,11 @@ namespace Core.Flash.Mvc
             while (messages.Count > 0)
             {
                 var message = messages.Dequeue();
-                var html = $@"<div class=""alert alert-{message.Type}"">{message.Text}</div>";
+
+                var html = message.Dismissable
+                    ? string.Format(HtmlDismissableTemplate, message.Type, message.Text)
+                    : string.Format(HtmlStandardTemplate, message.Type, message.Text);
+
                 output.Content.AppendHtml(html);
             }
         }
