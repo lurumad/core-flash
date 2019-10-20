40e51135eafbcc5a93df53203daf5acf8fac18d1
Anis Alibegić <anis.alibegic@gmail.com>
2019-10-20 12:47:08 +0200

    Added strongly-typed shortcuts for all types: FlashPrimary(), FlashSecondary(), FlashSuccess(), FlashDanger(), FlashWarning(), FlashInfo(), FlashLight(), FlashDark().

--- src/Core.Flash/Abstractions/IFlasher.cs
+++ src/Core.Flash/Abstractions/IFlasher.cs
@@ -3,6 +3,14 @@
     public interface IFlasher
     {
         void Flash(string type, string message, bool dismissable = false);
+        void FlashPrimary(string message, bool dismissable = false);
+        void FlashSecondary(string message, bool dismissable = false);
+        void FlashSuccess(string message, bool dismissable = false);
+        void FlashDanger(string message, bool dismissable = false);
+        void FlashWarning(string message, bool dismissable = false);
+        void FlashInfo(string message, bool dismissable = false);
+        void FlashLight(string message, bool dismissable = false);
+        void FlashDark(string message, bool dismissable = false);
 
         bool Any();
     }
--- src/Core.Flash/Flasher.cs
+++ src/Core.Flash/Flasher.cs
@@ -23,6 +23,47 @@ namespace Core.Flash
             tempData.Put(Constants.Key, messages);
             tempData.Save();
         }
+
+        public void FlashPrimary(string message, bool dismissable = false)
+
+        {
+            Flash(Types.Primary, message, dismissable);	
+        }
+
+        public void FlashSecondary(string message, bool dismissable = false)
+        {
+            Flash(Types.Secondary, message, dismissable);	
+        }
+
+        public void FlashSuccess(string message, bool dismissable = false)
+        {
+            Flash(Types.Success, message, dismissable);	
+        }
+
+        public void FlashDanger(string message, bool dismissable = false)
+        {
+            Flash(Types.Danger, message, dismissable);	
+        }
+
+        public void FlashWarning(string message, bool dismissable = false)
+        {
+            Flash(Types.Warning, message, dismissable);	
+        }
+
+        public void FlashInfo(string message, bool dismissable = false)
+        {
+            Flash(Types.Info, message, dismissable);	
+        }
+
+        public void FlashLight(string message, bool dismissable = false)
+        {
+            Flash(Types.Light, message, dismissable);	
+        }
+
+        public void FlashDark(string message, bool dismissable = false)
+        {
+            Flash(Types.Dark, message, dismissable);	
+        }
 	
         public bool Any()
         {
